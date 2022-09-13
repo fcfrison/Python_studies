be71f77a3fdae96cbaf8aaa828bd10b573963885
frison <fcfrison@gmail.com>
2022-09-13 10:55:19 -0300

    Update metafile

--- python_studies/etl_ideas/project2/src/extract.py
+++ python_studies/etl_ideas/project2/src/extract.py
@@ -6,15 +6,17 @@ load the data.
 '''
 import boto3
 import pandas as pd
-
+from collections import namedtuple
 from boto3.resources.factory import ServiceResource
 from boto3.resources import factory
-from datetime import datetime, timedelta
-from io import StringIO
+from datetime import date, datetime, timedelta
+from io import BytesIO, StringIO
+from typing import List
+
 import warnings
 warnings.filterwarnings("ignore")
 
-def read_csv_obj(bucket,obj,decoding)->pd.DataFrame:
+def read_csv_obj(bucket,obj,decoding:str='utf-8')->pd.DataFrame:
     '''
     Function that gets an specific S3 Bucket object and returns a 
     Pandas DataFrame. 
@@ -74,6 +76,86 @@ def extract(start_date:str, bucket_name:str, resource:str)->pd.DataFrame:
     bucket = s3.Bucket(f'{bucket_name}') # Specifying the chosen bucket
     df_all = csv_to_df(bucket,date_format,day_before)
     return df_all
+
+def return_date_list(arg_date:str, today_date:str, bucket_name:str, resource:str,
+              meta_key:str,date_format:str= "%Y-%m-%d")->List[date]:
+    '''
+    Function that takes 'arg_date' as the first day of the range of dates to be 
+    considered and 'today_date' as the last one.
+
+    Parameters
+    ---------------------
+        arg_date
+            A string with the first day of the range of dates to be considered;
+        today_date
+            A string with the last day of the range of dates to be considered;
+        date_format
+            The format used for the dates.
+    
+    Output
+    ---------------------
+        return_list
+            A list of dates.
+    '''
+    min_date = datetime.strptime(arg_date,date_format).date() - timedelta(days=1)
+    today = datetime.strptime(today_date,date_format).date()
+    try:
+        return_list =  [
+            (min_date + timedelta(days=x)) 
+            for x in range(0,(today-min_date).days + 1)
+        ]
+        df_meta, bucket = read_meta_data(bucket_name, resource,meta_key)
+        src_dates = set(pd.to_datetime(df_meta['source_date']).dt.date)
+        dates_missing = set(return_list[1:] ) - src_dates
+        if(dates_missing):
+            min_date = min(set(return_list[1:]) - src_dates) - timedelta(days=1)
+            return_dates = [date.strftime(date_format) for date in return_list if date>=min_date]
+            return_min_date = (min_date + timedelta(days=1)).strftime(date_format)
+        else:
+            return_dates = []
+            return_min_date = datetime(2200,1,1).date()
+    except bucket.session.client('s3').exceptions.NoSuchKey:
+        return_dates = [(min_date + timedelta(days=x)).strftime(date_format) 
+                    for x in range(0, (today-min_date).days) + 1]
+        return_min_date = arg_date
+    
+
+    return return_min_date, return_dates
+
+def read_meta_data(bucket_name:str, resource:str,meta_key:str)->pd.DataFrame:
+    '''
+    Function that read the metadata table and return a Pandas DataFrame from it.
+
+    Parameters
+    --------------------- 
+        bucket_name
+            A string with the name of the S3 Bucket where the 'metadata.csv' file
+            can be found.
+
+    '''
+    Obj = namedtuple('Obj',['key'])
+    obj = Obj(meta_key)
+    s3:ServiceResource = boto3.resource(f'{resource}') # create an instance of service resource
+    bucket = s3.Bucket(f'{bucket_name}') # Specifying the chosen bucket
+    df_meta = read_csv_obj(bucket,obj)
+    return df_meta, bucket
+
+def write_df_to_s3_csv(bucket_name:str,resource:str,df:pd.DataFrame, key)->bool:
+    s3:ServiceResource = boto3.resource(f'{resource}')
+    bucket = s3.Bucket(f'{bucket_name}')
+    out_buffer = StringIO()
+    df.to_csv(out_buffer,index=False)
+    bucket.put_object(Body=out_buffer.getvalue(),Key=key)
+    return True
+
+def update_meta_file(bucket_name:str, meta_key:str, resource:str, extract_date_list:List):
+    df_new = pd.DataFrame(columns=['source_date',' datetime_of_processing'])
+    df_new['source_date'] = extract_date_list
+    df_new[' datetime_of_processing'] = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    df_old, _ = read_meta_data(bucket_name,resource,meta_key)
+    df_all = pd.concat([df_old,df_new])
+    write_df_to_s3_csv(bucket_name,resource,df_all,meta_key)
+
 
 if(__name__=='__main__'):
     extract(start_date='2022-08-30', resource='s3', bucket_name='xetra-1234')
--- python_studies/etl_ideas/project2/src/main.py
+++ python_studies/etl_ideas/project2/src/main.py
@@ -4,27 +4,34 @@ import os
 
 from datetime import datetime
 
-from extract import extract
+from extract import extract, return_date_list, update_meta_file
 from transform import transform
 from load import load_bucket
-def main():
+def main(min_day:str):
     # basic configuration
-    date_format = "%Y-%m-%d"
     source_bucket = 'xetra-1234'
     target_bucket = os.getenv("AWS_BUCKET_NAME")
     resource_name = 's3'
     target_file = 'xetra_daily_report' + datetime.today().strftime("%Y%m%d_%H%M%S")
     file_format = '.parquet'
+    today = '2022-09-13'
+    meta_key ='meta_file.csv' 
+
     # extract data
-    df_all = extract(start_date=datetime.today().strftime(date_format), 
-                    resource=resource_name, bucket_name=source_bucket)
+    extract_date, date_list = return_date_list(min_day,today,target_bucket,resource_name,meta_key)
+    df_all = extract(start_date=extract_date, resource=resource_name, 
+                     bucket_name=source_bucket)
     # transform data
     df_all = transform(df_all)
     
     # load data
     load_bucket(target_file,file_format, df=df_all, resource=resource_name,
                 target_bucket=target_bucket)
+    update_meta_file(target_bucket,meta_key,resource_name,date_list)
+    
     print("File loaded.")
 
 if(__name__=="__main__"):
-    main()
+    date_format = "%Y-%m-%d"
+    min_day = datetime.today().strftime(date_format)
+    main(min_day = '2022-09-11')
